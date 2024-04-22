5d68554d40e1e452422d8c3e059bb0db733ca6f4
Thais Marques Mota <thaismferreira42@gmail.com>
2024-04-22 23:00:21 +0000

    criando arquivos da API

---
+++ models/contato.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+
+const contatoSchema = new mongoose.Schema({
+  nome: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  telefone: {
+    type: String,
+    required: true,
+  },
+  endereco: {
+    type: String,
+    required: true,
+  },
+  foto: {
+    type: String,
+    required: true,
+  },
+});
+
+const Contato = mongoose.model('Contato', contatoSchema);
