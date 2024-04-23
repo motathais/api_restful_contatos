a7b720ed825d67a20f6c8441a1c576e929daa48d
Thais Marques Mota <thaismferreira42@gmail.com>
2024-04-23 01:12:43 +0000

    alterados arquivos de teste

--- app.js
+++ app.js
@@ -10,6 +10,15 @@ app.use(express.json());
 const contatoRouter = require('./routes/contatoRoutes');
 app.use('/contatos', contatoRouter);
 
+
+app.use((req, res) =>{
+  res.statusCode=400;
+  res.send({
+    error:"123",
+    message:"Rota inválida"
+  })
+});
+
 mongoose.connect(process.env.MONGODB_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
